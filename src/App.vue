7cf35c87c591090725d3f4bab8cd19e38e74ac55
FouLin <foulin.tm@gmail.com>
2022-09-18 18:46:09 +0700

    Это шапка, и она готова

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
   <div id="app">
-
+    <header-site></header-site>
   </div>
 </template>
 
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import HeaderSite from "@/components/HeaderSite.vue";
+
+@Component({
+  components: {
+    HeaderSite
+  }
+})
+
+export default class App extends Vue {
+}
+</script>
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+h2 {
+  margin: 0;
+}
+
+p {
+  margin: 0;
+}
+
+body {
+  margin: 0;
+}
 </style>
--- src/components/HeaderSite.vue
+++ src/components/HeaderSite.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-
+  <div class="header-site">
+    <h2>
+      Тестовое задание
+    </h2>
   </div>
 </template>
 
-<script>
-export default  {
-  name: "HeaderSite"
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+
+export default class HeaderSite extends Vue{
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+h2{
+  margin-left: 110px;
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 42px;
+  color: #FFFFFF;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.header-site {
+  align-items: center;
+  display: flex;
+  height: 260px;
+  background: linear-gradient(267.21deg, #900793 0.16%, rgba(134, 4, 137, 0.785707) 0.16%, rgba(159, 12, 162, 0.762269) 43.22%, rgba(5, 1, 190, 0.734375) 98.12%, rgba(0, 28, 126, 0.619792) 100.15%, rgba(6, 77, 142, 0.447917) 102.03%, rgba(1, 0, 60, 0) 102.73%);
+}
 </style>
