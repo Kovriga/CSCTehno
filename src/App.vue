cd27366958dbf8247dd5c40dc658f3d274936b9d
FouLin <foulin.tm@gmail.com>
2022-09-18 18:57:17 +0700

    Разработка фильтра

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
     <header-site></header-site>
+    <div class="center-component-mesh">
+      <div class="filter">
+        <filter-table></filter-table>
+      </div>
+      <div class="table-data"></div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator";
 import HeaderSite from "@/components/HeaderSite.vue";
+import FilterTable from "@/components/FilterTable.vue";
 
 @Component({
   components: {
+    FilterTable,
     HeaderSite
   }
 })
@@ -19,6 +27,19 @@ export default class App extends Vue {
 </script>
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+#app {
+  height: 100vh;
+  background: #E5E5E5;
+}
+
+.center-component-mesh {
+  display: flex;
+}
+
+.filter, .table-data {
+
+}
 
 h2 {
   margin: 0;
---
+++ src/components/FilterTable.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="filter-component">
+    <h1>Я есть фильтр</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+
+export default class FilterTable extends Vue{
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
