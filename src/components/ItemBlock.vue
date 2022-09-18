0ec601f7755dc953e1cd1bd8e567b9df627d6d56
FouLin <foulin.tm@gmail.com>
2022-09-18 23:53:41 +0700

    Финал

--- src/components/ItemBlock.vue
+++ src/components/ItemBlock.vue
@@ -3,6 +3,7 @@
     <div class="card" v-for="item in tableDate" :key="item.id">
       <div class="header-card">
         <h3>{{item.id}}</h3>
+        <svg @click="deleteGoods(item)" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M13.05 42q-1.25 0-2.125-.875T10.05 39V10.5H8v-3h9.4V6h13.2v1.5H40v3h-2.05V39q0 1.2-.9 2.1-.9.9-2.1.9Zm21.9-31.5h-21.9V39h21.9Zm-16.6 24.2h3V14.75h-3Zm8.3 0h3V14.75h-3Zm-13.6-24.2V39Z"/></svg>
       </div>
       <div class="content-cart">
         <div>
@@ -72,14 +73,15 @@ export default class ItemBlock extends Vue{
     }
   }
 
+
+  deleteGoods(item: any):void {
+    this.tableDate.splice(this.tableDate.indexOf(item), 1);
+  }
+
   created(): void {
     this.axios.get('http://localhost:3000/posts').then((response: AxiosResponse) => {
       this.tableDate = response.data
     })
-  }
-
-  dateTime(item: string): string {
-    return item.slice()
   }
 
   typeLang(item: string): string {
@@ -152,6 +154,7 @@ export default class ItemBlock extends Vue{
   background: rgba(237, 232, 245, 0.5);
   height: 40px;
   width: 100%;
+  justify-content: space-between;
   h3 {
     margin: 16px 12px;
   }
--- src/components/TableCom.vue
+++ src/components/TableCom.vue
@@ -17,12 +17,8 @@
           <td>{{ item.number }}</td>
           <td>{{ typeLang(item.type) }}</td>
           <td>{{ dateTime(item.creationDate) }}</td>
-          <td>
-            <svg width="4" height="14" viewBox="0 0 4 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                  d="M4 1.75C4 0.7875 3.1 0 2 0C0.9 0 0 0.7875 0 1.75C0 2.7125 0.9 3.5 2 3.5C3.1 3.5 4 2.7125 4 1.75ZM4 12.25C4 11.2875 3.1 10.5 2 10.5C0.9 10.5 0 11.2875 0 12.25C0 13.2125 0.9 14 2 14C3.1 14 4 13.2125 4 12.25ZM4 7C4 6.0375 3.1 5.25 2 5.25C0.9 5.25 0 6.0375 0 7C0 7.9625 0.9 8.75 2 8.75C3.1 8.75 4 7.9625 4 7Z"
-                  fill="#647A8C"/>
-            </svg>
+          <td @click="deleteGoods(item)">
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M13.05 42q-1.25 0-2.125-.875T10.05 39V10.5H8v-3h9.4V6h13.2v1.5H40v3h-2.05V39q0 1.2-.9 2.1-.9.9-2.1.9Zm21.9-31.5h-21.9V39h21.9Zm-16.6 24.2h3V14.75h-3Zm8.3 0h3V14.75h-3Zm-13.6-24.2V39Z"/></svg>
           </td>
         </tr>
         </tbody>
@@ -43,7 +39,6 @@ export default class Table extends Vue {
 
   @Watch('$store.state.search')
   getSearch(): void {
-    console.log('z')
     this.axios.get('http://localhost:3000/posts', {
     }).then((response: AxiosResponse) => {
       this.tableDate = response.data.filter((item: any) => item.number.toLowerCase().indexOf(this.$store.state.search.toLowerCase()) > -1)
@@ -90,6 +85,10 @@ export default class Table extends Vue {
   }
 
   created(): void {
+    this.getItems()
+  }
+
+  getItems(): void {
     this.axios.get('http://localhost:3000/posts', {}).then((response: AxiosResponse) => {
       this.tableDate = response.data
     })
@@ -97,6 +96,10 @@ export default class Table extends Vue {
 
   dateTime(item: string): string {
     return item.slice()
+  }
+
+  deleteGoods(item: any):void {
+    this.tableDate.splice(this.tableDate.indexOf(item), 1);
   }
 
   typeLang(item: string): string {
